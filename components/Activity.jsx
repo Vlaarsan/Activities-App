64b6f796d5e8ada0c28de1df79324b33cd658e16
Vlaarsan <priamdavidpro@gmail.com>
2024-01-09 12:58:30 +0100

    ok

--- components/Activity.jsx
+++ components/Activity.jsx
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: 170,
-    height: 100,
+    height: 110,
     backgroundColor: "black",
     padding: 25,
     margin: 15,
